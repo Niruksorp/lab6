9a36e8fbbb4c806a9bdc8e53c3a1f240c2814aea
Niruksorp <prge200000@mail.ru>
2020-12-04 15:16:22 +0700

    added registration page and logic using BCryptPasswordEncoder

--- src/main/java/com/example/lab4/OldApplicationController.java
+++ src/main/java/com/example/lab4/OldApplicationController.java
@@ -1,8 +1,14 @@
 package com.example.lab4;
 
 import com.example.lab4.Entity.Jewel;
+import com.example.lab4.Entity.Role;
+import com.example.lab4.Entity.User;
+import com.example.lab4.Repository.UserRepo;
 import com.example.lab4.Service.CRUDJewel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import javax.xml.validation.Validator;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
+
 @Controller
 public class OldApplicationController {
 
@@ -18,7 +27,33 @@ public class OldApplicationController {
     public OldApplicationController(CRUDJewel crudJewel) {
         this.crudJewel = crudJewel;
     }
+    @Autowired
+    private UserRepo usrRepo;
 
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        User user = new User();
+        model.addAttribute(user);
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(@ModelAttribute("user") User user) {
+        Optional<User> usrFromDb = usrRepo.findByUsername(user.getUsername());
+
+        if (usrFromDb.isPresent()) {
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        PasswordEncoder passwordEncoders = new BCryptPasswordEncoder();
+
+        user.setPassword(passwordEncoders.encode(user.getPassword()));
+        usrRepo.save(user);
+
+        return "redirect:/login";
+    }
     @GetMapping
     public String menu(){
         return "menu";
--- src/main/java/com/example/lab4/config/WebSecurityConfig.java
+++ src/main/java/com/example/lab4/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.lab4.Entity.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
